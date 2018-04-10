ddb525d3cc636057d861031c6b9f3b46f3e1cba6
Matheus Jacques <ml.jacques@hotmail.com>
2018-04-09 22:51:18 -0300

    Adicionado arquivos ao GitHub

---
+++ logintwo/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Login from './app/components/Login';
+import Profile from './app/components/Profile';
+
+const Applicattion = StackNavigator({
+    Home: { screen: Login },
+    Profile: { screen: Profile }
+    }, {
+      navigationOptions: {
+        header: false,
+      }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Applicattion />
+    );
+  }
+}
